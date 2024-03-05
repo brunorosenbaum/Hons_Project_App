af77c3f602b27ae6614c8ff120694649a3d1960e
brunorosenbaum11 <2000512@uad.ac.uk>
2024-03-05 21:28:37 +0000

    Candidate and phi calc functions added

--- Coursework/Coursework/RATIONAL_SOLVER.cpp
+++ Coursework/Coursework/RATIONAL_SOLVER.cpp
@@ -1,5 +1,8 @@
 #include "RATIONAL_SOLVER.h"
 
+#include <iostream>
+#include <ostream>
+
 #define ETA 2 //Eta (H or squiggly n) parameter
 #define POW_OF_RHO 3 //Rho is P, or p parameter. 
 
@@ -19,22 +22,13 @@ eta_(ETA), power_of_Rho_(POW_OF_RHO), gridSize_(0)
 
 RATIONAL_SOLVER::~RATIONAL_SOLVER()
 {
-	std::vector< CELL_DERV* >::iterator itr = all_Cells.begin();
-	while (itr != all_Cells.end())
-	{
-		if(*itr) //Safe delete
-		{
-			delete* itr;
-			*itr = nullptr; 
-		}
-		++itr;
-	}
-	boundary_Cells.clear();
-	positive_Cells.clear();
-	negative_Cells.clear();
-	candidate_Cells.clear();
-	all_Cells.clear();
+	ClearVectors(); 
 
+}
+
+bool RATIONAL_SOLVER::LoadMap(const std::string& path)
+{
+	return false;
 }
 
 void RATIONAL_SOLVER::CreateBoundaryCells() //I'm assuming this method creates the boundary grid, akin to the green channel
@@ -173,4 +167,153 @@ void RATIONAL_SOLVER::CalcPositivePotential()
 		positive_Cells[iIndex].potential = positivePhi;
 		iIndex++; 
 	}
+}
+
+bool RATIONAL_SOLVER::InitializeGrid(const std::string& path)
+{
+	ClearVectors();
+	bool result = LoadMap(path);
+	if(result)
+	{
+		CreateBoundaryCells();//Create boundary grid cells (just position, coords)
+		CalcBoundaryPotential(); //Calc phi of all cells.
+		CalcPositivePotential();//Negative is not needed since cells default phi is 0. 
+
+		//Generate initial candidate map
+		CreateCandidateMap(); 
+		//And calculate phi for candidate cells (here's the rational method)
+
+	}
+	return result; 
+}
+
+void RATIONAL_SOLVER::CreateCandidateMap() //Map refers to the C++ MAP DATA STRUCTURE.
+//Maps index of all cells vector to elements of candidate cells vector.
+{
+	candidateMap_DS.clear();
+	int iIndex;
+	int cellX, cellY; //All cells vector x and y pos
+	int candidateX, candidateY; //Candidate cells vector x and y pos
+	int iChildIndex;
+
+	std::vector< CELL_DERV >::iterator itr = negative_Cells.begin(); //Go through negative cells
+	while (itr != negative_Cells.end())
+	{
+		cellX = (*itr).center[0]; 
+		cellY = (*itr).center[1];
+		iIndex = cellY * gridSize_ + cellX;
+
+		if(all_Cells[iIndex]) //Go through all cells
+		{
+			//Go through neighbors of each cell & update the candidates' xy coords to that
+			for(auto n : *all_Cells[iIndex]->neighbors)
+			{
+				//Candidate's xPos = current cell's xPos + n neighbor's xPos
+				candidateX = cellX + all_Cells[n]->neighbors[n]->center[0];
+				//Candidate's yPos = current cell's xPos + n neighbor's yPos
+				candidateX = cellX + all_Cells[n]->neighbors[n]->center[0];
+				iChildIndex = cellY * gridSize_ + cellX;
+
+				//If cells xy coords are within the gridsize
+				if(cellX >= 0 && cellX < gridSize_ && cellY >= 0 && cellY < gridSize_ && all_Cells[iChildIndex])
+				{
+					if(all_Cells[iChildIndex]->state == EMPTY) //If cell is empty
+					{
+						//Find each iChildIndex element in candidate cell map
+						std::map<int, CELL_DERV*>::iterator candidate_Itr = candidateMap_DS.find(iChildIndex);
+						if(candidate_Itr == candidateMap_DS.end())
+						{
+							//Insert cell at the end of the map
+							candidateMap_DS.insert(std::map<int, CELL_DERV*>::value_type(iChildIndex, all_Cells[iChildIndex]));
+						}
+					}
+				}
+
+			}
+		}
+		++itr; 
+	}
+
+
+}
+
+void RATIONAL_SOLVER::CalcPotential_Rational()
+{
+	unsigned int totalCells = gridSize_ * gridSize_; //256 cells
+	if(all_Cells.size() < totalCells)
+	{
+		//Cell size error
+		std::cout << "cell size error !! " << std::endl;
+		return; 
+	}
+
+	// calculate electric potential (Phi) for only candidate cells
+	float phi; 
+	float r; //TODO: I HAVE NO CLUE ABOUT R
+	float B, N, P; //Boundary, negative, positive phi. These are the ones from the formula.
+
+	int mapKey; //Value that'll be assigned to the int map key of the candidate cells map.
+	CELL_DERV* current_Cell;
+
+	if(candidateMap_DS.size() == 0)
+	{
+		std::cout << "There is no candidate cells to compute electric potential !!" << std::endl;
+		return;
+	}
+
+	//Now, go through candidate cells
+	auto mapItr = candidateMap_DS.begin();
+	while(mapItr != candidateMap_DS.end())
+	{
+		//Assign to map values
+		mapKey = mapItr->first;
+		current_Cell = mapItr->second;
+
+		if(current_Cell && current_Cell->state == EMPTY) //If current cell is empty
+		{
+			// -----------------------------------------------------------
+			// for boundaries, use pre-computed values
+			B = boundary_Cells[mapKey].potential;
+			// -----------------------------------------------------------
+			// for positive charges, use pre-computed value
+			P = positive_Cells[mapKey].potential;
+			// -----------------------------------------------------------
+			// for negative charge cells (phi = 0)
+			N = 0;
+
+			//TODO: I DON'T UNDERSTAND THE CLUSTERED CELLS THING 8(
+			//TODO: AND R IS NEEDED TO CALCULATE P TT_TT
+			//r = CalcDistance()
+
+
+			//HERE IT IS!! THIS IS THE FORMULA ON THE PAPER PHI = P / (N X B)
+			phi = (1.0f / B) * (1.0f / N) * P;
+
+			current_Cell->N_ = N;
+			current_Cell->P_ = P;
+			current_Cell->B_ = B;
+			current_Cell->potential = phi;
+		}
+		++mapItr; 
+	}
+
+}
+
+void RATIONAL_SOLVER::ClearVectors()
+{
+	std::vector< CELL_DERV* >::iterator itr = all_Cells.begin();
+	while (itr != all_Cells.end())
+	{
+		if (*itr) //Safe delete
+		{
+			delete* itr;
+			*itr = nullptr;
+		}
+		++itr;
+	}
+	boundary_Cells.clear();
+	positive_Cells.clear();
+	negative_Cells.clear();
+	candidate_Cells.clear();
+	all_Cells.clear();
 }
--- Coursework/Coursework/RATIONAL_SOLVER.h
+++ Coursework/Coursework/RATIONAL_SOLVER.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <map>
+#include <string>
 #include <vector>
 
 #include "CELL_DERV.h"
@@ -9,6 +11,7 @@ public:
 	RATIONAL_SOLVER(); 
 	~RATIONAL_SOLVER();
 
+	bool LoadMap(const std::string& path); 
 	//Pre-computation functions
 	void CreateBoundaryCells(); //Create boundary grid cells (just position, coords)
 	// pre-computation of electric potential for each type of cells
@@ -17,11 +20,18 @@ public:
 	//Hear me out: we do NOT need to initialize negative potentials for the negative cells
 	//Bc both base constructors initialize potential value to 0. 
 
+	//Computation funcs
+	bool InitializeGrid(const std::string& path);
+	void CreateCandidateMap();
+	void CalcPotential_Rational(); 
+
 private:
+	//Helper funcs
 	float	CalcDistance(float x1, float y1, float x2, float y2) const //Vector magnitude (distance) between 2 points
 	{
 		return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 	}
+	void ClearVectors(); 
 
 private:
 	//Should we make these cell_derv* instead? 
@@ -29,8 +39,12 @@ private:
 	std::vector<CELL_DERV> positive_Cells; //With >0 potential (phi > 0)
 	std::vector<CELL_DERV> negative_Cells; //With 0 potential (phi == 0)
 
-	std::vector<CELL_DERV> candidate_Cells;
 	std::vector<CELL_DERV*> all_Cells;
+
+	//Candidates
+	std::map<int, CELL_DERV*> candidateMap_DS; //Map DATA STRUCTURE for each candidate cell. Each has a different position. This is for potential calcs.
+	std::vector<CELL_DERV> candidate_Cells; //Vector of candidate cells. They may have the same position, but each has a different parent.This is for selecting next lightning branch growth.  
+
 
 	// initial state
 	CELL_DERV* startPoint_Cell; //Initial negative charge
